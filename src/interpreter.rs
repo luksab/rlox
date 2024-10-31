1d3d54efca8395675a886f8f5911f0b1d538ec52
luksab <lukas@sabatschus.de>
2024-10-31 16:00:32 +0100

    refactor to use same structure as ci

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -26,108 +26,117 @@ impl InterpreterInstance {
 
     pub fn tokenize(&mut self, source: &str) {
         let mut tokens = Vec::new();
-        for (line_num, line) in source.lines().enumerate() {
-            let chars: Vec<_> = line.chars().collect();
-            let mut i = 0;
-            while i < chars.len() {
-                let char = chars[i];
-                match char {
-                    // handle the easy cases first
-                    '(' | ')' | '{' | '}' | '*' | '.' | ',' | '+' | '-' | ';' => {
-                        tokens.push(Token {
-                            inner: match char {
-                                '(' => TokenType::LeftParen,
-                                ')' => TokenType::RightParen,
-                                '{' => TokenType::LeftBrace,
-                                '}' => TokenType::RightBrace,
-                                '*' => TokenType::Star,
-                                '.' => TokenType::Dot,
-                                ',' => TokenType::Comma,
-                                '+' => TokenType::Plus,
-                                '-' => TokenType::Minus,
-                                ';' => TokenType::Semicolon,
-                                _ => unreachable!(),
-                            },
-                            lexeme: char.to_string(),
-                            line: line_num + 1,
-                            start_column: i + 1,
-                            length: 1,
-                        });
-                        i += 1;
-                    }
-                    // '=', '!', '<', and '>' can be single or double characters (e.g. '==' or '=')
-                    '=' | '<' | '>' | '!' => {
-                        let next_char = chars.get(i + 1);
-                        if let Some(&next_char) = next_char {
-                            let (len, token_type) = match (char, next_char) {
-                                ('=', '=') => (2, TokenType::EqualEqual),
-                                ('!', '=') => (2, TokenType::BangEqual),
-                                ('<', '=') => (2, TokenType::LessEqual),
-                                ('>', '=') => (2, TokenType::GreaterEqual),
-                                ('=', _) => (1, TokenType::Equal),
-                                ('!', _) => (1, TokenType::Bang),
-                                ('<', _) => (1, TokenType::Less),
-                                ('>', _) => (1, TokenType::Greater),
-                                _ => {
-                                    self.error(line_num + 1, "Unexpected character");
-                                    (0, TokenType::EOF)
-                                }
-                            };
-                            tokens.push(Token {
-                                inner: token_type,
-                                lexeme: chars[i..i + len].iter().collect(),
-                                line: line_num + 1,
-                                start_column: i + 1,
-                                length: len,
-                            });
-                            i += len;
-                        } else {
-                            let token_type = match char {
-                                '=' => TokenType::Equal,
-                                '!' => TokenType::Bang,
-                                '<' => TokenType::Less,
-                                '>' => TokenType::Greater,
-                                _ => unreachable!(),
-                            };
-                            tokens.push(Token {
-                                inner: token_type,
-                                lexeme: char.to_string(),
-                                line: line_num + 1,
-                                start_column: i + 1,
-                                length: 1,
-                            });
-                            i += 1;
-                        }
-                    }
-                    // slash needs to be handled separately because it can be a comment
-                    '/' => {
-                        let next_char = chars.get(i + 1);
-                        if let Some(&next_char) = next_char {
-                            if next_char == '/' {
-                                // just skip the rest of the line
-                                break;
+        let chars = source.chars().collect::<Vec<char>>();
+
+        let mut line = 0;
+        let mut i = 0;
+        while i < chars.len() {
+            let char = chars[i];
+            match char {
+                // handle the easy cases first
+                '(' | ')' | '{' | '}' | '*' | '.' | ',' | '+' | '-' | ';' => {
+                    tokens.push(Token {
+                        inner: match char {
+                            '(' => TokenType::LeftParen,
+                            ')' => TokenType::RightParen,
+                            '{' => TokenType::LeftBrace,
+                            '}' => TokenType::RightBrace,
+                            '*' => TokenType::Star,
+                            '.' => TokenType::Dot,
+                            ',' => TokenType::Comma,
+                            '+' => TokenType::Plus,
+                            '-' => TokenType::Minus,
+                            ';' => TokenType::Semicolon,
+                            _ => unreachable!(),
+                        },
+                        lexeme: char.to_string(),
+                        line: line + 1,
+                        start_column: i + 1,
+                        length: 1,
+                    });
+                    i += 1;
+                }
+                // '=', '!', '<', and '>' can be single or double characters (e.g. '==' or '=')
+                '=' | '<' | '>' | '!' => {
+                    let next_char = chars.get(i + 1);
+                    if let Some(&next_char) = next_char {
+                        let (len, token_type) = match (char, next_char) {
+                            ('=', '=') => (2, TokenType::EqualEqual),
+                            ('!', '=') => (2, TokenType::BangEqual),
+                            ('<', '=') => (2, TokenType::LessEqual),
+                            ('>', '=') => (2, TokenType::GreaterEqual),
+                            ('=', _) => (1, TokenType::Equal),
+                            ('!', _) => (1, TokenType::Bang),
+                            ('<', _) => (1, TokenType::Less),
+                            ('>', _) => (1, TokenType::Greater),
+                            _ => {
+                                self.error(line + 1, "Unexpected character");
+                                (0, TokenType::EOF)
                             }
-                        }
+                        };
                         tokens.push(Token {
-                            inner: TokenType::Slash,
+                            inner: token_type,
+                            lexeme: chars[i..i + len].iter().collect(),
+                            line: line + 1,
+                            start_column: i + 1,
+                            length: len,
+                        });
+                        i += len;
+                    } else {
+                        let token_type = match char {
+                            '=' => TokenType::Equal,
+                            '!' => TokenType::Bang,
+                            '<' => TokenType::Less,
+                            '>' => TokenType::Greater,
+                            _ => unreachable!(),
+                        };
+                        tokens.push(Token {
+                            inner: token_type,
                             lexeme: char.to_string(),
-                            line: line_num + 1,
+                            line: line + 1,
                             start_column: i + 1,
                             length: 1,
                         });
-                        i += 1;
-                    }
-                    // ignore whitespace
-                    ' ' | '\t' | '\r' => {
-                        i += 1;
-                    }
-                    char => {
-                        self.error(line_num + 1, &format!("Unexpected character: {}", char));
                         i += 1;
                     }
                 }
+                // slash needs to be handled separately because it can be a comment
+                '/' => {
+                    let next_char = chars.get(i + 1);
+                    if let Some(&next_char) = next_char {
+                        if next_char == '/' {
+                            // just skip the rest of the line
+                            while i < chars.len() && chars[i] != '\n' {
+                                i += 1;
+                            }
+                            continue;
+                        }
+                    }
+                    tokens.push(Token {
+                        inner: TokenType::Slash,
+                        lexeme: char.to_string(),
+                        line: line + 1,
+                        start_column: i + 1,
+                        length: 1,
+                    });
+                    i += 1;
+                }
+                // ignore whitespace
+                ' ' | '\t' | '\r' => {
+                    i += 1;
+                }
+                // newlines need to be handled separately to keep track of the current line
+                '\n' => {
+                    line += 1;
+                    i += 1;
+                }
+                char => {
+                    self.error(line + 1, &format!("Unexpected character: {}", char));
+                    i += 1;
+                }
             }
         }
+
         tokens.push(Token {
             inner: TokenType::EOF,
             lexeme: String::new(),
