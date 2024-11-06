745c895e9fe9b74179298b20cd0e46f819ff5afe
luksab <lukas@sabatschus.de>
2024-11-06 10:29:44 +0100

    done with codecrafters; re-add String+Other

--- src/interpreter/eval/mod.rs
+++ src/interpreter/eval/mod.rs
@@ -190,9 +190,8 @@ impl Eval for Binary {
                 (Literal::String(l), Literal::String(r)) => {
                     Ok(Literal::String(format!("{}{}", l, r)))
                 }
-                // comment this back in, when done with codecrafters
-                // (Literal::String(l), other) => Ok(Literal::String(format!("{}{}", l, other))),
-                // (other, Literal::String(r)) => Ok(Literal::String(format!("{}{}", other, r))),
+                (Literal::String(l), other) => Ok(Literal::String(format!("{}{}", l, other))),
+                (other, Literal::String(r)) => Ok(Literal::String(format!("{}{}", other, r))),
                 _ => Err(ExecError {
                     message: "Operands must be two numbers or two strings".to_string(),
                     range: self.left.range.merge(&self.right.range),
